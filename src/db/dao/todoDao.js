a757881b1e918783a9c63a964ddcd3a8449a0bfc
Praveen Singh <praveensingh.name@gmail.com>
2023-04-24 01:22:03 +0530

    idb introduction

--- src/db/dao/todoDao.js
+++ src/db/dao/todoDao.js
@@ -3,47 +3,21 @@ import { DB, TODO_STORE, TXN_WRITE } from '../db';
 class TodoDao {
     async add(todo) {
         const db = await DB.openDB();
-
-        return new Promise((resolve, reject) => {
-            const store = db.transaction([TODO_STORE], TXN_WRITE).objectStore(TODO_STORE);
-            const request = store.add(todo);
-
-            request.onsuccess = () => {
-                const todoKey = request.result;
-
-                this.get(todoKey).then((todoDb) => {
-                    resolve(todoDb);
-                });
-            };
-        });
+        const store = db.transaction([TODO_STORE], TXN_WRITE).objectStore(TODO_STORE);
+        const todoKey = await store.add(todo);
+        return store.get(todoKey);
     }
 
     async getAll() {
         const db = await DB.openDB();
-
-        return new Promise((resolve, reject) => {
-            const store = db.transaction([TODO_STORE]).objectStore(TODO_STORE);
-            const request = store.getAll();
-
-            request.onsuccess = () => {
-                const todos = request.result;
-                resolve(todos);
-            };
-        });
+        const store = db.transaction([TODO_STORE]).objectStore(TODO_STORE);
+        return store.getAll();
     }
 
     async get(key) {
         const db = await DB.openDB();
-        return new Promise((resolve, reject) => {
-            const store = db.transaction([TODO_STORE]).objectStore(TODO_STORE);
-            const request = store.get(key);
-
-            request.onsuccess = () => {
-                const todoDb = request.result;
-
-                resolve(todoDb);
-            };
-        });
+        const store = db.transaction([TODO_STORE]).objectStore(TODO_STORE);
+        return store.get(key);
     }
 }
 
--- src/db/db.js
+++ src/db/db.js
@@ -1,34 +1,20 @@
+import * as idb from 'idb';
+
 export const TODO_STORE = 'todos';
 export const DB_NAME = 'todo_db';
 export const DB_VERSION = 1;
-
-
-// 
 
 export const TXN_WRITE = 'readwrite';
 
 class DB {
     static openDB() {
-        return new Promise((resolve, reject) => {
-            const request = window.indexedDB.open(DB_NAME, DB_VERSION);
-
-            request.onerror = () => {
-                reject(new Error('No permission to open DB!'));
-            };
-
-            request.onsuccess = (event) => {
-                const db = event.target.result;
-                resolve(db);
-            };
-
-            request.onupgradeneeded = (event) => {
-                const db = event.target.result;
+        return idb.openDB(DB_NAME, DB_VERSION, {
+            upgrade(db) {
                 const store = db.createObjectStore(TODO_STORE, { keyPath: 'id', autoIncrement: true });
-
                 store.transaction.oncomplete = () => {
                     console.log(`Store ${TODO_STORE} has been created`);
                 };
-            };
+            },
         });
     }
 }
